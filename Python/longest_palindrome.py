4b07139f40530a334715e7840d1d32548c389865
nesterovmaxim31 <maxgoonfuture@gmail.com>
2025-02-08 22:54:13 +0300

    Unfinished longest_palindrome

--- Python/longest_palindrome.py
+++ Python/longest_palindrome.py
@@ -1,13 +1,27 @@
 def longest_palindrome(s):
-    if (len(s) == 0):
+    length = len(s)
+    mxlen = 1
+    
+    if (length == 0):
         return 0
-    if (len(s) == 1):
+    if (length == 1):
         return 1
 
-    for i in  
+    for i in range(0, length):
+        for j in range(0, length - i):
+            if (s[i:length - i - j + 1] == \
+                s[i:length - i - j + 1][::-1] \
+                and len(s[i:length - i - j + 1])>=2):
+                print(s[i:length - i - j + 1], i, j, -i-j+1)
+                mxlen = max(mxlen, length - j - i + 1)
+
+
+    return mxlen
+    
 
 
 if (__name__ == "__main__"):
-    s = input();
-    print(longest_palindrome(s));
+    while(True):
+        s = input();
+        print(longest_palindrome(s));
     
