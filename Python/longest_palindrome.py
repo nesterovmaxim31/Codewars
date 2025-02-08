78e877fbdaea6d1c5790f1e9f596495f988e6ec2
nesterovmn <maxgoonfuture@gmail.com>
2025-02-08 20:57:49 +0300

    not finished Longest palindrom python

---
+++ Python/longest_palindrome.py
@@ -0,0 +1,13 @@
+def longest_palindrome(s):
+    if (len(s) == 0):
+        return 0
+    if (len(s) == 1):
+        return 1
+
+    for i in  
+
+
+if (__name__ == "__main__"):
+    s = input();
+    print(longest_palindrome(s));
+    
