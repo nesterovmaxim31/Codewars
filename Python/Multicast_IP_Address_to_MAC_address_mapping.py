e95f2421d8227befd168b997da24a3f8fdd15158
nesterovmaxim31 <maxgoonfuture@gmail.com>
2025-03-09 01:21:08 +0300

    Added link for Multicast IP Address to MAC address mapping. Realization in Python

--- Python/Multicast_IP_Address_to_MAC_address_mapping.py
+++ Python/Multicast_IP_Address_to_MAC_address_mapping.py
@@ -3,7 +3,7 @@ def mcast_ip_to_mac(ip_string):
     return f"01:00:5E:{hex(int(l[1]) & 0x7f)[2:]:0>2}:{hex(int(l[2]))[2:]:0>2}:\
 {hex(int(l[3]))[2:]:0>2}".upper() 
 
-
+# https://www.codewars.com/kata/5a9d43ceba1bb52492000080/python
 if __name__ == "__main__":
     print(mcast_ip_to_mac("224.128.0.1"))
     print(mcast_ip_to_mac("224.0.0.6"))
