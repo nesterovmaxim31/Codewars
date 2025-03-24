85ad6bfbeb814a2d23fd79d4964d098d8434379e
nesterovmn <maxgoonfuture@gmail.com>
2025-03-24 12:31:16 +0300

    Add code for Twice linear on Golang

--- Go/Twice_linear.go
+++ Go/Twice_linear.go
@@ -2,83 +2,51 @@ package main
 
 import "fmt"
 
+func DblLinear(n int) int {
+	if n == 0 {
+		return 1;
+	} else if n == 1 {
+		return 3;
+	} else if n == 2 {
+		return 4;
+	}
 
-type MinVal struct {
-	minval int
-	minval_i int
-}
+	values_y := []int{3}
+	values_z := []int{4}
+	var val, val_y, val_z int
 
-// check in this value already exist in slice
-func is_in_slice(allvals []int, value int) bool {
-	for i := 0; i < len(allvals); i++ {
-		if allvals[i] == value {
-			return true
+	for i := 0; i < n - 1; i++ {
+		if values_y[0] < values_z[0] {
+			val = values_y[0]
+			values_y = values_y[1:]
+		} else if values_y[0] > values_z[0] {
+			val = values_z[0]
+			values_z = values_z[1:]
+		} else if values_y[0] == values_z[0] {
+			val = values_z[0]
+			values_z = values_z[1:]
+			values_y = values_y[1:]
 		}
+		
+		val_y = val * 2 + 1
+		val_z = val * 3 + 1
+
+		values_y = append(values_y, val_y)
+		values_z = append(values_z, val_z)
 	}
 
-	return false
-}
-
-
-func get_minval(allvals []int) MinVal {
-	var ans MinVal
-	ans.minval = allvals[0]
-	
-	for i := 0; i < len(allvals); i++ {
-		if (allvals[i] < allvals[ans.minval_i]) {
-			ans.minval_i = i
-			ans.minval = allvals[i]
-		}
+	if values_y[0] <= values_z[0] {
+		return values_y[0]
+	} else {
+		return values_z[0]
 	}
-
-	return ans
-}
-
-
-// replace minimal value with two new and return replaced value
-func replace_minval(allvals []int) []int {
-	// find min value
-	var minval, minval_i int
-	ans := get_minval(allvals)
-	minval = ans.minval
-	minval_i = ans.minval_i
-	
-	if (is_in_slice(allvals, minval * 2 + 1) != true) {
-		allvals[minval_i] =  minval * 2 + 1
-	} else if (is_in_slice(allvals, minval * 3 + 1) != true) {
-		allvals[minval_i] =  minval * 3 + 1
-	}
-
-	if (is_in_slice(allvals, minval * 3 + 1) != true) {
-		allvals = append(allvals, minval * 3 + 1)
-	}
-
-	
-	return allvals
-}
-
-
-func DblLinear (n int) int {
-	allvals := []int{1}
-
-	for i := 0; i < n; i++ {
-		allvals = replace_minval(allvals)
-	}
-
-	return get_minval(allvals).minval
 }
 
 func main() {
-	fmt.Println("For 10:", DblLinear(10)) // should be equal to 22
-	fmt.Println("For 50:", DblLinear(50))
-	fmt.Println("For 100:", DblLinear(100))
-	fmt.Println("For 500:", DblLinear(500))
-	fmt.Println("For 1000:", DblLinear(1000))
-	
+	fmt.Println(DblLinear(10)) // should be equal to 22
+	fmt.Println(DblLinear(50))
+	fmt.Println(DblLinear(100))
+	fmt.Println(DblLinear(500))
+	fmt.Println(DblLinear(1000))
+	fmt.Println(DblLinear(100000))
 }
-/*
-   dotest(50, 175)
-   dotest(100, 447)
-   dotest(500, 3355)
-   dotest(1000, 8488)
-*/
